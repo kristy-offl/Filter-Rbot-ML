3b6643f759c1b6e5e17dd7813df61f7ae3eac115
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-04-14 14:36:44 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """<b>Hello {},
 
 I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+There is no limits for my filtering :)
 
 See <i>/help</i> for commands and more details.</b>
 """
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @ML_BotUpdates</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Filter ML Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @ML_BotUpdates    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
